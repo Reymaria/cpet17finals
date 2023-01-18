b452fb12dc30766c4be3d4d144e815a94facb58a
Corvus Necromyst <ivanhayz.gonzales@gsfe.tupcavite.edu.ph>
2023-01-18 20:05:27 +0800

    Wrap database query in try-block

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -12,18 +12,21 @@ export default withSessionRoute(async (req, res) => {
   const password = req.body.password;
 
   console.log("\x1b[31;4m/!\\ aaaa /!\\: src/pages/api/login.js:14\x1b[0m");
-  const query = await pool.query("SELECT * FROM users WHERE user = ?", [username]);
+  try {
+    const query = await pool.query("SELECT * FROM users WHERE user = ?", [username]);
+  } catch (error) {
+    return res.send({ok: false});
+  }
+  
   const db_user = query.user;
   const db_hash = query.hash;
-    
-  // uncomment this line once db is supplied; for now, username and password field is both '...'
+  
   if (username === db_user && bcrypt.hashSync(password, salt) === db_hash) {
-  /// if (username === db_user && password === db_hash) {
     req.session.user = username;
     await req.session.save();
-    
-    return res.status(201).send({ok: true});
+
+    return res.send({ok: true});
   }
-    
-  return res.status(403).send('');
+  
+  return res.send({ok: false});
 })
